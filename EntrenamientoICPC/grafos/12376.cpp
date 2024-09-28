0e1477e5782d42d21486afbece033c65307ae2de
renegz <00077321@uca.edu.sv>
2024-09-27 22:37:20 -0600

    :zap: uva 12376

--- EntrenamientoICPC/grafos/12376.cpp
+++ EntrenamientoICPC/grafos/12376.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 #define vi vector<int>
 
+
 vi kw;
+vector<vi> AL(100);
+int counter = 0;
+int position = 0;
+void solve(int s);
 
 int main (){
     int T;
     cin >> T;
 
     int n, m;
-    while(T--){
+    for(int i = 1; i <= T; i++){
         cin >> n >> m;
         for (int i = 0; i < n; i++)
         {
@@ -20,7 +25,6 @@ int main (){
             kw.push_back(val);
         }
 
-        vector<vi> AL(n);
 
         while (m--)
         {
@@ -29,8 +33,30 @@ int main (){
             AL[index].push_back(conection);
         }
 
+        solve(0);
+        cout << "Case " << i << ": " << counter << " " << position << endl;
+        counter = 0;
+        position = 0;
+
+        kw.clear();
     }
     
     
     
+}
+
+void solve(int s){
+    int aux = 0;
+    if(AL[s].size() == 0) {
+        return;
+    }
+    for (int i = 0; i<AL[s].size(); i++){
+        if(counter + kw[AL[s][i]] > aux) {
+            aux = counter + kw[AL[s][i]];
+            position = AL[s][i];
+        }
+    }
+    counter = aux;
+    AL[s].clear();
+    solve(position);
 }
